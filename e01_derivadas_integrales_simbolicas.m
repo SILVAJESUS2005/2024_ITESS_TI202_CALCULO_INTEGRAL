e8613b06da8c2d16dc082029aa594942297afb24
Jesus Lopez <jesuslopezsilva206@gmail.com>
2024-05-06 12:08:37 -0600

    inicio del repositorio calculo

---
+++ e01_derivadas_integrales_simbolicas.m
@@ -0,0 +1,9 @@
+%%  ITESS-TICS
+%%  Enero-Junio 2024
+%%  TI-202 CALCULO INTEGRAL
+%%  Francisco Javier Montecillo Puente
+%%  Jesus Lopez Silva
+%%  Derivadas e integrales simbolicas.
+%%  06/05/2024
+pkg load symbolic
+syms x
